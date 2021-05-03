26c641e66210ee1a2b3e09e90433327097c017f6
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-05-03 14:32:49 +0200

    added function to js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-console.log("Cześć Wszystkim! Wpadajcie na wyzwanie, kto NIE wpada ten PARÓWKA!! Ten kto wpada, ten wygrywa");
+{
+    const welcome = () => {
+    console.log("Cześć Wszystkim! Wpadajcie na wyzwanie, kto NIE wpada ten PARÓWKA!! Ten kto wpada, ten wygrywa");
+}
+
+const onChangeBacgroundClick = () => {
+    container.classList.toggle("newBackground");
+};
+
+welcome();
+
 
 let changeBackgroundButton= document.querySelector(".changeBackgroundButton");
 let container = document.querySelector(".container");
 
-changeBackgroundButton.addEventListener("click", () => {
-    container.classList.toggle("newBackground");
+changeBackgroundButton.addEventListener("click", onChangeBacgroundClick);
     
 
-});
+}
+
