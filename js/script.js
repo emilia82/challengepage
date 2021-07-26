9c588787683939626b6bcd1dea6a47d10ae66099
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-26 21:00:01 +0200

    function of image changed added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-{
+
     const welcome = () => {
-    console.log("Cześć Wszystkim! Wpadajcie na wyzwanie, kto NIE wpada ten PARÓWKA!! Ten kto wpada, ten wygrywa");
-}
-
-const onChangeBacgroundClick = () => {
-    container.classList.toggle("newBackground");
-};
-
-welcome();
-
-
-let changeBackgroundButton= document.querySelector(".changeBackgroundButton");
-let container = document.querySelector(".container");
-
-changeBackgroundButton.addEventListener("click", onChangeBacgroundClick);
+        console.log("Cześć Wszystkim! Wpadajcie na wyzwanie, kto NIE wpada ten PARÓWKA!! Ten kto wpada, ten wygrywa");
     
-
 }
 
+{
+    const newBackgroundColour = () => {
+        const container = document.querySelector(".container");
+        container.classList.toggle("newBackgroundColour");
+    };
+
+    const init = () => {
+        let changeBackgroundColourButton = document.querySelector(".changeBackgroundColourButton");
+     
+        changeBackgroundColourButton.addEventListener("click", "newBackgroundColour");
+    };
+
+    let myImage = document.querySelector("img");
+    myImage.onclick = function () {
+
+        let mySrc = myImage.getAttribute("src");
+        if (mySrc === "images/Emilia-Wyzwanie.jpg") {
+            myImage.setAttribute("src", "images/Emilia-Challenger.jpg");
+        } else {
+            myImage.setAttribute("src", "images/Emilia-Challenger.jpg");
+        }
+    };
+
+    init();
+
+}
